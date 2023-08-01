f136066c7b93cacfaf6a95b533dae51542662d28
Kavooce <kavooce.cheetah@gmail.com>
2023-07-31 20:41:00 -0500

    update domain

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -1,5 +1,10 @@
 export default {
   secret: process.env.SECRET_KEY!,
   publish: process.env.PUBLISH_KEY!,
-  domain: 'http://localhost:3000',
+  domain:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3000'
+      : process.env.NODE_ENV === 'production'
+      ? 'https://stripe-payment-mocha.vercel.app/'
+      : '',
 };
